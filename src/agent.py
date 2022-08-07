ebdeaec39ed1067541b346a4f71ad144649306b9
Alex Tuisov <queldelan@gmail.com>
2022-08-07 15:44:53 +0300

    Tickled the pickle of pl DQNAgent

--- src/agent.py
+++ src/agent.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 from replay_buffer import ReplayBuffer
 import numpy as np
+import torch
+from networks import AtariScreenNetwork
 
 
 class Agent(ABC):
@@ -18,13 +20,75 @@ class RandomAgent(Agent):
 
 
 class DQNAgent(Agent):
-    def __init__(self, network, optimizer, size_of_buffer, batch_size):
-        self.network = network
-        self.optimizer = optimizer
-        self.replay_buffer = ReplayBuffer(size_of_buffer, batch_size)
+    """
+    Base Agent class handling the interaction with the environment
+    Args:
+        env: training environment
+        replay_buffer: replay buffer storing experiences
+    """
+
+    def __init__(self, net: torch.nn.Module, replay_buffer: ReplayBuffer) -> None:
+        self.net = net
+        self.replay_buffer = replay_buffer
+        self.parameters = {}
+        self._set_parameters()
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    def _set_parameters(self):
+        self.parameters["exploration_method"] = "epsilon_greedy"
+        self.parameters["epsilon"] = 0.1
 
     def act(self, state, reward):
+        action = self._get_action()
+        self._learning_step()
+        return action
+
+    def _get_action(self) -> int:
+        """
+        Using the given network, decide what action to carry out
+        using an epsilon-greedy policy
+        Args:
+        Returns:
+            action
+        """
+        if self.parameters["exploration_method"] == "epsilon_greedy" and np.random.random() < self.parameters["epsilon"]:
+            action = self.env.action_space.sample()
+        else:
+            state = torch.tensor([self.state])
+
+            if self.device not in ['cpu']:
+                state = state.cuda(self.device)
+
+            q_values = self.net(state)
+            _, action = torch.max(q_values, dim=1)
+            action = int(action.item())
+
+        return action
+
+    def _learning_step(self) -> None:
         pass
 
-    def learn_batch(self):
-        pass
+    # def play_step(self, net: torch.nn.Module, epsilon: float = 0.0, device: str = 'cpu') -> Tuple[float, bool]:
+    #     """
+    #     Carries out a single interaction step between the agent and the environment
+    #     Args:
+    #         net: DQN network
+    #         epsilon: value to determine likelihood of taking a random action
+    #         device: current device
+    #     Returns:
+    #         reward, done
+    #     """
+    #
+    #     action = self.get_action(net, epsilon, device)
+    #
+    #     # do step in the environment
+    #     new_state, reward, terminal, _ = self.env.step(action)
+    #
+    #     exp = self.state, action, reward, terminal, new_state
+    #
+    #     self.replay_buffer.append(exp)
+    #
+    #     self.state = new_state
+    #     if terminal:
+    #         self.reset()
+    #     return reward, terminal
--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
 import gym
 from agent import RandomAgent
 from stable_baselines3.common.env_util import AtariWrapper
+from pl_bolts.models.rl import DQN
 
 def main():
     env = gym.make("Atlantis-v4")
     env = AtariWrapper(env)
     observation = env.reset()
+    done = False
     agent = RandomAgent(env.action_space.n)
-    info = 0
-    for _ in range(10000):
+    for _ in range(1000):
         env.render(mode="rgb_array")
-        action = agent.act(observation, info)  # User-defined policy function
-        observation, reward, done, info = env.step(action)
+        action = agent.act(observation, done)  # User-defined policy function
+        observation, reward, done, _ = env.step(action)
         if done:
             observation = env.reset()
     env.close()
