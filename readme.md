7baf68023e075097d0865aa65e4af6359460fdfd
Alex Tuisov <queldelan@gmail.com>
2022-08-08 13:43:17 +0300

    updated readme.md

--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 # An RL fun project
+
+Requirements:
+
+- gym[atari]
+- pytorch lighting 
+- pl_bolts
--- src/wrappers.py
+++ src/wrappers.py
@@ -1,3 +1,8 @@
+"""
+From stable baselines code
+"""
+
+
 import numpy as np
 import os
 os.environ.setdefault('PATH', '')
